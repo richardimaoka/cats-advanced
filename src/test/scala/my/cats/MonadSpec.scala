d338e89aa6fb88054799f28693deea8ad18faa33
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-09-24 10:47:58 +0900

    Some more monad law tests

--- src/test/scala/my/cats/MonadSpec.scala
+++ src/test/scala/my/cats/MonadSpec.scala
@@ -1,5 +1,6 @@
 package my.cats
 
+import org.scalacheck.Gen
 import org.scalatest._
 import org.scalatest.prop.PropertyChecks
 
@@ -34,9 +35,6 @@ class MonadSpec extends FlatSpec with Matchers with PropertyChecks {
   }
 
   "Monad" should "satisfy associativity" in {
-    import cats.instances.option._
-    import cats.syntax.applicative._
-
     val m = Option(30)
 
     val f: Int => Option[String] = i => Some((i * 2).toString + " yeah")
@@ -50,15 +48,6 @@ class MonadSpec extends FlatSpec with Matchers with PropertyChecks {
 
     left should be (right)
   }
-
-
-//  def checkLeftIdentity[A, B, M[_]: Monad[M]](f: A => M[A]): Unit = forAll {
-//    import cats.instances.option._
-//    import cats.syntax.applicative._
-//
-//    val aMonad = a.pure[M[A]]
-//      aMonad..flatMap(f) should be(f(a))
-//  }
 
   /***********************************
    * Using Generator for Option
@@ -105,7 +94,7 @@ class MonadSpec extends FlatSpec with Matchers with PropertyChecks {
       logit(s"testing for $a")
       logit(s"f(a) = ${f(a)} should be a.pure[F].flatMap(f) = ${a.pure[F].flatMap(f)}")
     }
-    a.pure[F].flatMap(f) should be(f(a))
+    a.pure[F].flatMap(f) should be (f(a))
   }
 
   def verifyRightIdentityLaw[A, F[_]](m: F[A], log: Boolean = false)(implicit applicative: Monad[F]): Unit = {
@@ -115,7 +104,7 @@ class MonadSpec extends FlatSpec with Matchers with PropertyChecks {
     if(log) {
       logit(s"m.flatMap(_.pure) = ${m.flatMap(_.pure)} should be m = ${m}")
     }
-    m.flatMap(_.pure) should be(m)
+    m.flatMap(_.pure) should be (m)
   }
 
   def verifyAssociativityLaw[A, B, C, F[_]](m: F[A], log: Boolean = false)(f: A => F[B], g: B => F[C])(implicit applicative: Monad[F]): Unit = {
@@ -131,22 +120,15 @@ class MonadSpec extends FlatSpec with Matchers with PropertyChecks {
   def runNamedTest(name: String, test: => Unit): Unit = {
     logit("running test = " + name)
     test
+    logit("fnished " + name)
   }
 
-  "Monad[Option[Int]]" should "satisfy left identity 2" in {
+  "Monad[Option[Int]]" should "satisfy left identity with verifiers" in {
     import cats.instances.option._
-
-    case class MyException(val message: String) extends Exception {
-      override def equals(that: Any): Boolean = {
-        this.getClass == that.getClass &&
-        this.message == that.asInstanceOf[MyException].message
-      }
-    }
 
     val f0: Int => Option[String] = i => Some((i * 2).toString + " yeah")
     val f1: Int => Option[String] = _ => None
     val f2: Int => Option[Int] = i => Some(i*i)
-
     /**
      * The Java default Exception's equals method returns true,
      * only when they are exactly the same instance (memory reference)
@@ -154,38 +136,182 @@ class MonadSpec extends FlatSpec with Matchers with PropertyChecks {
      * So, using my own Exception type which only checks the equality of the error message
      *  => test passes
      */
+    case class MyException(val message: String) extends Exception {
+      override def equals(that: Any): Boolean = {
+        this.getClass == that.getClass &&
+          this.message == that.asInstanceOf[MyException].message
+      }
+    }
     val f3: Int => Option[Throwable] = i => Some(new MyException(s"Exception: $i"))
     val f4: Int => Option[MyException] = i => Some(new MyException(s"Exception: $i"))
-
+    /**
+     * In contrast, this one with the Java default Exception is not run for test as it will fail.
+     * You can mix in CatsSuite which overrides catsCanEqual via StrictCatsEquality trait,
+     * and that allows you call === calling underlying Cats Eq's equality
+     */
+    //val f: Int => Option[Throwable] = i => Some(new Exception(s"Exception: $i"))
     val f5: Int => Option[Either[Throwable, String]] = i =>
       if(i % 2 == 0)
         Some(Right(s"even number ${i} allowed"))
       else
         Some(Left(new MyException(s"odd number ${i} is not allowed ")))
     val f6: Int => Option[List[Int]] = i => Some((0 to i).toList)
-    val f7: Int => Option[String] =
-      i =>
-        Map(
-          0 -> "here",
-          2 -> "here",
-          4 -> "here",
-          5 -> "here",
-          6 -> "here",
-          11 -> "here",
-          13 -> "here",
-          -12 -> "here",
-          10 -> "here"
-        ).get(i)
+    val f7: Int => Option[String] = i => Map( 0 -> "here", 2 -> "here", 4 -> "here", 5 -> "here" ).get(i)
 
-    forAll { (a: Int) => {
-      runNamedTest("test0", verifyLeftIdentityLaw(a)(f0))
-      runNamedTest("test1", verifyLeftIdentityLaw(a)(f1))
-      runNamedTest("test2", verifyLeftIdentityLaw(a)(f2))
-      runNamedTest("test3", verifyLeftIdentityLaw(a)(f3))
-      runNamedTest("test4", verifyLeftIdentityLaw(a)(f4))
-      runNamedTest("test5", verifyLeftIdentityLaw(a)(f5))
-      runNamedTest("test6", verifyLeftIdentityLaw(a)(f6))
-      runNamedTest("test7", verifyLeftIdentityLaw(a)(f7))
-    }}
+    runNamedTest("test0", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f0)})
+    runNamedTest("test1", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f1)})
+    runNamedTest("test2", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f2)})
+    runNamedTest("test3", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f3)})
+    runNamedTest("test4", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f4)})
+    runNamedTest("test5", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f5)})
+    val upto100 = for(n <- Gen.choose(0, 100)) yield n
+    runNamedTest("test6", forAll(upto100){(a: Int) => verifyLeftIdentityLaw(a)(f6)})
+    runNamedTest("test7", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f7)})
+  }
+
+  "Monad[Option[Int]]" should "satisfy right identity with verifiers" in {
+    import cats.instances.option._
+
+    runNamedTest("test0", forAll{(a: Int) => verifyRightIdentityLaw(Option(a))})
+    runNamedTest("test1", forAll{(a: Int) => verifyRightIdentityLaw(None)})
+    runNamedTest("test2", forAll{(a: Option[Int]) => verifyRightIdentityLaw(a)})
+    runNamedTest("test3", forAll{(a: Option[String]) => verifyRightIdentityLaw(a)})
+    runNamedTest("test4", forAll{(a: Option[Long]) => verifyRightIdentityLaw(a)})
+    runNamedTest("test5", forAll{(a: Option[Double]) => verifyRightIdentityLaw(a)})
+    val upto100 = for(n <- Gen.choose(0, 100)) yield n
+    runNamedTest("test6", forAll(upto100){(a: Int) => verifyRightIdentityLaw(Option((0 to a).toList))})
+    runNamedTest("test7", forAll{(a: Option[Option[Int]]) => verifyRightIdentityLaw(a)})
+
+    case class MyException(val message: String) extends Exception {
+      override def equals(that: Any): Boolean = {
+        this.getClass == that.getClass &&
+          this.message == that.asInstanceOf[MyException].message
+      }
+    }
+    runNamedTest("test8", forAll{(a: String) => verifyRightIdentityLaw(Option(new MyException(a)))})
+  }
+
+  "Monad[Option[Int]]" should "satisfy associativity with verifiers" in {
+    import cats.instances.option._
+
+    val f1: Int => Option[Double] = i => Some(i)
+    val g1: Double => Option[String] = i => Some(i.toString)
+
+    val f2: Int => Option[Double] = i => None
+    val g2: Double => Option[String] = i => None
+
+    val f3: Int => Option[Option[Double]] = o => Some(Some(o))
+    val g3: Option[Double] => Option[Option[String]] = o => o match {
+      case Some(d) => Some(Some(d.toString))
+      case None => None
+    }
+
+    val f4: Int => Option[Double] = i => Some(i*i)
+    val g4: Double => Option[String] = i => Some(i.toString)
+
+    case class MyException(val message: String) extends Exception {
+      override def equals(that: Any): Boolean = {
+        this.getClass == that.getClass &&
+          this.message == that.asInstanceOf[MyException].message
+      }
+    }
+    val f5: Int => Option[Throwable] = i => Some(new MyException(s"Exception: $i"))
+    val g5: Throwable => Option[String] = i => Some(i.toString)
+
+    val f6: Int => Option[MyException] = i => Some(new MyException(s"Exception: $i"))
+    val g6: MyException => Option[String] = i => Some(i.toString)
+
+    /**
+     * In contrast, this one with the Java default Exception is not run for test as it will fail.
+     * You can mix in CatsSuite which overrides catsCanEqual via StrictCatsEquality trait,
+     * and that allows you call === calling underlying Cats Eq's equality
+     */
+    //val f: Int => Option[Throwable] = i => Some(new MyException(s"Exception: $i"))
+    //val g: Throwable => Option[String] = i => Some(i.toString)
+
+    val f7: Int => Option[Either[Throwable, String]] = i =>
+      if(i % 2 == 0)
+        Some(Right(s"even number ${i} allowed"))
+      else
+        Some(Left(new MyException(s"odd number ${i} is not allowed ")))
+    val g7: Either[Throwable, String] => Option[String] =
+      either => either.fold(
+        throwable => Some("you've got a throwable!" + throwable.getMessage),
+        string => Some("congrats! here's your string = " + string)
+      )
+
+    runNamedTest("test1", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f1, g1)})
+    runNamedTest("test2", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f2, g2)})
+    runNamedTest("test3", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f3, g3)})
+    runNamedTest("test4", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f4, g4)})
+    runNamedTest("test5", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f5, g5)})
+    runNamedTest("test6", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f6, g6)})
+    runNamedTest("test7", forAll{(a: Int) => verifyAssociativityLaw(Option(a))(f7, g7)})
+  }
+
+  "Monad[List[Int]]" should "satisfy left identity with verifiers" in {
+    import cats.instances.list._
+
+    val f0: Int => List[Int] = i => (1 to i).toList
+    val f1: Int => List[Int] = _ => List[Int]()
+    val f2: Int => List[String] = i => List(i.toString)
+    /**
+     * The Java default Exception's equals method returns true,
+     * only when they are exactly the same instance (memory reference)
+     *
+     * So, using my own Exception type which only checks the equality of the error message
+     *  => test passes
+     */
+    case class MyException(val message: String) extends Exception {
+      override def equals(that: Any): Boolean = {
+        this.getClass == that.getClass &&
+          this.message == that.asInstanceOf[MyException].message
+      }
+    }
+    val f3: Int => List[Throwable] = i => List(new MyException(s"Exception: $i"))
+    val f4: Int => List[MyException] = i => List(new MyException(s"Exception: $i"))
+    /**
+     * In contrast, this one with the Java default Exception is not run for test as it will fail.
+     * You can mix in CatsSuite which overrides catsCanEqual via StrictCatsEquality trait,
+     * and that allows you call === calling underlying Cats Eq's equality
+     */
+    //val f: Int => List[Throwable] = i => List(new Exception(s"Exception: $i"))
+    val f5: Int => List[Either[Throwable, String]] = i =>
+      if(i % 2 == 0)
+        List(Right(s"even number ${i} allowed"))
+      else
+        List(Left(new MyException(s"odd number ${i} is not allowed ")))
+    val f6: Int => List[List[Int]] = i => List((0 to i).toList)
+
+    val upto100 = for(n <- Gen.choose(0, 100)) yield n
+    runNamedTest("test0", forAll(upto100){(a: Int) => verifyLeftIdentityLaw(a)(f0)})
+    runNamedTest("test1", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f1)})
+    runNamedTest("test2", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f2)})
+    runNamedTest("test3", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f3)})
+    runNamedTest("test4", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f4)})
+    runNamedTest("test5", forAll{(a: Int) => verifyLeftIdentityLaw(a)(f5)})
+    runNamedTest("test6", forAll(upto100){(a: Int) => verifyLeftIdentityLaw(a)(f6)})
+  }
+
+  "Monad[List[Int]]" should "satisfy right identity with verifiers" in {
+    import cats.instances.list._
+
+    runNamedTest("test0", forAll{(a: Int) => verifyRightIdentityLaw(List(a))})
+    runNamedTest("test1", forAll{(a: Int) => verifyRightIdentityLaw(Nil)})
+    runNamedTest("test2", forAll{(a: List[Int]) => verifyRightIdentityLaw(a)})
+    runNamedTest("test3", forAll{(a: List[String]) => verifyRightIdentityLaw(a)})
+    runNamedTest("test4", forAll{(a: List[Long]) => verifyRightIdentityLaw(a)})
+    runNamedTest("test5", forAll{(a: List[Double]) => verifyRightIdentityLaw(a)})
+    val upto100 = for(n <- Gen.choose(0, 100)) yield n
+    runNamedTest("test6", forAll(upto100){(a: Int) => verifyRightIdentityLaw((0 to a).toList)})
+    runNamedTest("test7", forAll{(a: List[Option[Int]]) => verifyRightIdentityLaw(a)})
+
+    case class MyException(val message: String) extends Exception {
+      override def equals(that: Any): Boolean = {
+        this.getClass == that.getClass &&
+          this.message == that.asInstanceOf[MyException].message
+      }
+    }
+    runNamedTest("test8", forAll{(a: String) => verifyRightIdentityLaw(List(new MyException(a)))})
   }
 }
